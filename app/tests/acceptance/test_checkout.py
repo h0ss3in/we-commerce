0dd9f82a7430090ff136930a53add643a578eccc
Hossein Hashemi <hhashemi@pyango.ch>
2022-11-18 11:11:27 +0330

    Add checkout acceptance test

---
+++ app/tests/acceptance/test_checkout.py
@@ -0,0 +1,24 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_checkout_price_without_discount():
+    response = client.post(
+        "/checkout/",
+        json={
+            "items": [
+                "001",
+                "002",
+                "001",
+                "004",
+                "003"
+            ]
+        }
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "price": 360
+    }
