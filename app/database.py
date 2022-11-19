1c45ec55c7665b358477a64bec44d736d458c558
Hossein Hashemi <hhashemi@pyango.ch>
2022-11-19 13:56:30 +0330

    Move products data to products of database

--- app/database.py
+++ app/database.py
@@ -1,26 +1,28 @@
-products = {
-    "001": {
-        "name": "Rolex",
-        "unit_price": 100,
-        "discount": {
-            "amount_required": 3,
-            "discounted_price": 200
-        }
-    },
-    "002": {
-        "name": "Michael Kors",
-        "unit_price": 80,
-        "discount": {
-            "amount_required": 2,
-            "discounted_price": 120
-        }
-    },
-    "003": {
-        "name": "Swatch",
-        "unit_price": 50
-    },
-    "004": {
-        "name": "Casio",
-        "unit_price": 30
-    },
+database = {
+    "products": {
+        "001": {
+            "name": "Rolex",
+            "unit_price": 100,
+            "discount": {
+                "amount_required": 3,
+                "discounted_price": 200
+            }
+        },
+        "002": {
+            "name": "Michael Kors",
+            "unit_price": 80,
+            "discount": {
+                "amount_required": 2,
+                "discounted_price": 120
+            }
+        },
+        "003": {
+            "name": "Swatch",
+            "unit_price": 50
+        },
+        "004": {
+            "name": "Casio",
+            "unit_price": 30
+        },
+    }
 }
