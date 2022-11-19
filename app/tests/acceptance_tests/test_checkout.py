d315a77f465eb5ace0456ca3e2473c3944d5267a
Hossein Hashemi <hhashemi@pyango.ch>
2022-11-19 15:01:19 +0330

    Add acceptance tests
    Add test for checkout items with discounted prices
    Add test for checkout no item
    Add test for checkout single item
    Add test for checkout not existing item

--- app/tests/acceptance_tests/test_checkout.py
+++ app/tests/acceptance_tests/test_checkout.py
@@ -20,3 +20,65 @@ def test_checkout_price_without_discount():
     assert response.json() == {
         "price": 360
     }
+
+
+def test_checkout_price_with_discount():
+    response = client.post(
+        "/checkout/",
+        json=[
+            "001",
+            "002",
+            "001",
+            "004",
+            "001",
+            "003",
+            "001",
+            "002",
+            "001",
+            "003",
+            "001",
+            "004",
+            "001",
+        ]
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "price": 780
+    }
+
+
+def test_checkout_no_items():
+    response = client.post(
+        "/checkout/",
+        json=[]
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "price": 0
+    }
+
+
+def test_checkout_single_item():
+    response = client.post(
+        "/checkout/",
+        json=[
+            "001"
+        ]
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "price": 100
+    }
+
+
+def test_checkout_not_existing_item():
+    response = client.post(
+        "/checkout/",
+        json=[
+            "005"
+        ]
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "price": 100
+    }
