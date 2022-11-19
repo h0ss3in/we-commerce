a25a7251f8d14995b48640e0d6bd4f082c059601
Hossein Hashemi <hhashemi@pyango.ch>
2022-11-19 14:07:35 +0330

    Implement init_checkout_line and mock calculate_total_price methods

--- app/dependencies.py
+++ app/dependencies.py
@@ -11,14 +11,19 @@ class CheckoutHandler:
 
     def create(self):
         self.create_checkout_lines()
-        # TODO: create the checkout
-        self.total_price = 360
+        self.calculate_total_price()
 
     def create_checkout_lines(self):
         for item_id in self.item_ids:
             if item_id not in self.checkout_lines:
-                self.checkout_lines[item_id] = {
-                    **self.db["products"][item_id],
-                    "amount": 0
-                }
+                self.init_checkout_line(item_id)
             self.checkout_lines[item_id]["amount"] += 1
+
+    def calculate_total_price(self):
+        self.total_price = 360
+
+    def init_checkout_line(self, item_id):
+        self.checkout_lines[item_id] = {
+            **self.db["products"][item_id],
+            "amount": 0
+        }
